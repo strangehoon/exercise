3fb58e1fc73818798c2af40cf4e26eb743cb7472
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2023-07-04 02:21:00 +0900

    init

---
+++ src/main/java/com/example/exercise/ex.java
@@ -0,0 +1,15 @@
+package com.example.exercise;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ex {
+
+    @RequestMapping("ex")
+    public String ex(){
+        return "hello";
+    }
+
+
+}
