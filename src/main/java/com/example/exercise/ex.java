b28e70067b47a43d1f46a1f5010bac82f9dfdb0b
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2023-07-09 16:24:25 +0900

    Update

--- src/main/java/com/example/exercise/ex.java
+++ src/main/java/com/example/exercise/ex.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ex {
 
-    @RequestMapping("ex")
+    @RequestMapping("/ex")
     public String ex(){
         return "hello!!!";
     }
