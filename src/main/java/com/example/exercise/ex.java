b6208660fc3b9377fb26f058ac9ecb9975b798ee
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2023-07-12 21:21:20 +0900

    Update

--- src/main/java/com/example/exercise/ex.java
+++ src/main/java/com/example/exercise/ex.java
@@ -1,6 +1,7 @@
 package com.example.exercise;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ex {
 
     private final UserService userService;
 
     @GetMapping("/ex1")
     public String ex(){
+        log.info("1");
         User user = new User();
         user.setContent("hello");
         userService.saveUser(user);
