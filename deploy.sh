6edd9da6db492c1636031eb44b22d8936ecbfaca
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2023-07-14 22:48:18 +0900

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # 작업 디렉토리를 /home/ec2-user/app으로 변경
 cd /home/ubuntu
 
@@ -9,7 +7,7 @@ DOCKER_APP_NAME=exercise
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
@@ -22,7 +20,7 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
 	# docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
-	sudo docker-compose -f docker-compose.blue.yml up -d --build
+	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
   # 30초 동안 대기
   sleep 30
@@ -31,7 +29,7 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
   # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
-  sudo docker-compose -f docker-compose.green.yml down
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 
    # 사용하지 않는 이미지 삭제
   sudo docker image prune -af
@@ -41,12 +39,12 @@ if [ -z "$EXIST_BLUE" ]; then
 # blue가 실행중이면 green up
 else
 	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-	sudo docker-compose -f docker-compose.green.yml up -d --build
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
   sleep 30
 
   echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-  sudo docker-compose -f docker-compose.blue.yml down
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
   sudo docker image prune -af
 
   echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
@@ -55,4 +53,4 @@ fi
   echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
   echo "===================== 배포 완료 =====================" >> /home/ubuntu/deploy.log
-  echo >> /home/ubuntu/deploy.log
+  echo >> home/ubuntu/deploy.log
