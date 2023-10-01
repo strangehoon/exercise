72a1fbcba1f9a9b2c918119f1b1354b32bb69e05
DESKTOP-71386SR\hal8h <strangehoon2@gmail.com>
2023-10-01 11:31:22 +0900

    Update

--- src/main/java/com/example/exercise/ex.java
+++ src/main/java/com/example/exercise/ex.java
@@ -21,7 +21,7 @@ public class ex {
 
     @RequestMapping("/ex2")
     public String ex2(){
-        return "hellos hello";
+        return "hellos hello!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 
 //    @GetMapping("/ex3")
